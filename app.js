3a6b1391cd55d2410c789d3f16b571acd7ed1a1b
Santhosh <santhoshm12052003@gmail.com>
2021-09-22 21:53:38 +0530

    completed

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const arrowL = document.querySelector('.fa-caret-left')
 const about = document.querySelector('#about')
 const events = document.querySelector('#events')
 const findUs = document.querySelector('#findUs')
+const aboutLink = document.querySelector('#aboutL')
+const eventLink = document.querySelector('#eventL')
+const findLink = document.querySelector('#findUsL')
 arrowL.style.opacity = '0';
 arrowR.addEventListener('click', () => {
     if (about.style.display != 'none') {
@@ -28,5 +31,28 @@ arrowL.addEventListener('click', () => {
         arrowR.style.opacity = 1;
     }
 
+
+})
+eventLink.addEventListener('click', () => {
+    about.style.display = 'none'
+    events.style.display = 'block'
+    findUs.style.display = 'none'
+    arrowL.style.opacity = 1;
+    arrowR.style.opacity = 1;
+})
+findLink.addEventListener('click', () => {
+    about.style.display = 'none'
+    events.style.display = 'none'
+    findUs.style.display = 'block'
+    arrowL.style.opacity = 1;
+    arrowR.style.opacity = 0;
+
+})
+aboutLink.addEventListener('click', () => {
+    about.style.display = 'block'
+    events.style.display = 'none'
+    findUs.style.display = 'none'
+    arrowL.style.opacity = 0;
+    arrowR.style.opacity = 1;
 
 })
